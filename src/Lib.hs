bd507dda364c998bbfdce69ce1995e5dc04db8eb
Vincent Jousse <vincent@jousse.org>
2017-11-28 07:44:55 +0100

    Setup stack

---
+++ src/Lib.hs
@@ -0,0 +1,23 @@
+module Lib
+    ( picnic
+    ) where
+
+import Data.List
+import Text.Regex
+import System.Random
+import Data.Ord
+
+type Point     = (Float,Float)
+type Color     = (Int,Int,Int)
+type Polygon   = [Point]
+type Person    = [Int]
+type Link      = [Point]
+type Placement = [(Point,Person)]
+
+type EnergyFunction a              = a -> Int
+type TemperatureFunction           = Int -> Int -> Float
+type TransitionProbabilityFunction = Int -> Int -> Float -> Float
+type MotionFunction a              = StdGen -> a -> (StdGen,a)
+
+picnic :: IO ()
+picnic = putStrLn "Hello World! Let's have a picnic! \n"
